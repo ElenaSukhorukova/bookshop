f1ade1698ce86f8aaa15a61289ac606f4e448691
Elena Sukhorukova <suhorukova.el@gamil.com>
2025-02-14 12:32:08 +0400

    add karafka

--- core/app/models/concerns/enum_lists.rb
+++ core/app/models/concerns/enum_lists.rb
@@ -1,7 +1,5 @@
 module EnumLists
   extend ActiveSupport::Concern
 
-  USER_ROLES = %w[
-    customer employee admin
-  ].freeze
+  USER_ROLES = %w[employee admin].freeze
 end
--- core/app/models/user.rb
+++ core/app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   # customer employee admin
   attribute :role, :enum
   enum :role, array_to_enum_hash(EnumLists::USER_ROLES), prefix: :role, validate: true
+
+  before_save :validate_uniq_admin_role
+
+  scope :admin_user, -> { where(role: 'admin') }
+
+  private
+
+  def validate_uniq_admin_role
+    if User.admin_user.present?
+      abort "It should be only one admin!"
+    end
+  end
 end
